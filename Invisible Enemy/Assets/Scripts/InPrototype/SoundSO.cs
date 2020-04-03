0f31f45c6ef7011e5d96c15011fe2492202bab7d
PersonHumanDevelops <58825139+PersonHumanDevelops@users.noreply.github.com>
2020-04-03 14:01:42 -0600

    added lighting start

---
+++ Invisible Enemy/Assets/Scripts/InPrototype/SoundSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Sound Catagory", menuName = "SoundCatagory")]
+public class SoundSO : ScriptableObject
+{
+    public string ok;
+}
