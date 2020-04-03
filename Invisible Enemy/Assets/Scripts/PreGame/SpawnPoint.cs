2ee065a2331ef77369bc1f55ddcad663c47ab92c
PersonHumanDevelops <58825139+PersonHumanDevelops@users.noreply.github.com>
2020-04-03 10:50:40 -0600

    Added timer and spawnpoint system

---
+++ Invisible Enemy/Assets/Scripts/PreGame/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+	public Transform spawnPoint;
+	public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.transform.position = spawnPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
