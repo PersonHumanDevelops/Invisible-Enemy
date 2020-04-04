e58dbbc1965a6ec8ee181d47cb4002bfc8e3cb01
PersonHumanDevelops <58825139+PersonHumanDevelops@users.noreply.github.com>
2020-04-04 09:10:47 -0600

    im done writing names for these lol

---
+++ Invisible Enemy/Assets/Scripts/Stress/Shake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public Animator anim;
+    void Update()
+    {
+        
+    }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.collider.tag == "Player")
+		{
+			anim.SetBool("Shake?", true);
+		}
+	}
+	
+}
