0310fa541506b95bbe9b451cc1eba63c2377cca7
Gabriel Doyon <bfdgab@hotmail.com>
2020-04-04 20:05:32 -0400

    Pushing my Moemvent Script

--- Invisible Enemy/Assets/Scripts/Movement/Movement.cs
+++ Invisible Enemy/Assets/Scripts/Movement/Movement.cs
@@ -7,19 +7,34 @@ public class Movement : MonoBehaviour
 	public float speed;
 	public float jumpHeight;
 	public bool isGrounded = false;
+    private SpriteRenderer isFlipping;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-	
-	
+
+    private void Awake()
+    {
+        // get a reference to the SpriteRenderer component on this gameObject
+        isFlipping = GetComponent<SpriteRenderer>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
 		Jump();
 		transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-		
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isFlipping.flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            isFlipping.flipX = false;
+        }
     }
 	
 	void Jump()
@@ -30,6 +45,6 @@ public class Movement : MonoBehaviour
 		}
 		
 	}
-	
+
 	 
 }
